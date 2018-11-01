2fa52972c9511f5cb299d448c0b1c61ac25519a2
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-01 10:14:15 -0400

    adds create table statements

---
+++ schema.sql
@@ -0,0 +1,19 @@
+-- should only ocntain the create table statements
+
+create table players (
+    id serial primary key,
+    name varchar (100)
+);
+
+create table games (
+    id serial primary key,
+
+    player1_score integer,
+    player2_score integer,
+
+    winner_id integer references players (id),
+
+    -- These are "foreign keys"
+    player1_id integer references players (id),
+    player2_id integer references players (id)
+);
